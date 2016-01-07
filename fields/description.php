124af807f9c231340f9e3d639a31a3e79001b347
Dmitry Rekun <bzzjuka@gmail.com>
2016-01-07 22:32:31 +0200

    Update copyright to 2016

--- fields/description.php
+++ fields/description.php
@@ -2,7 +2,7 @@
 /**
  * @package    Jmb_Tree
  * @author     Sherza & Dmitry Rekun <support@norrnext.com>
- * @copyright  Copyright (C) 2012 - 2015 NorrNext. All rights reserved.
+ * @copyright  Copyright (C) 2012 - 2016 NorrNext. All rights reserved.
  * @license    GNU General Public License version 3 or later; see license.txt
  */
 
--- tmpl/dark.php
+++ tmpl/dark.php
@@ -2,7 +2,7 @@
 /**
  * @package    Jmb_Tree
  * @author     Artem Valchuk <support@norrnext.com>
- * @copyright  Copyright (C) 2012 - 2015 NorrNext. All rights reserved.
+ * @copyright  Copyright (C) 2012 - 2016 NorrNext. All rights reserved.
  * @license    GNU General Public License version 3 or later; see license.txt
  */
 
